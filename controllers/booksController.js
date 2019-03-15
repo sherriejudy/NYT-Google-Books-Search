e31825c0ddf2837c11bda08b306af159e1f04da1
Judy Hu <judy.hu9@gmail.com>
2019-03-14 20:51:48 -0400

    thursday class

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,7 +5,7 @@ module.exports = {
   findAll: function(req, res) {
     db.Book.find(req.query)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.send(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
